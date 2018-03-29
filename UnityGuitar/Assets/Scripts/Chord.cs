e242364de6a78078c31171f6da2c1c4a4134e1cd
Michael <mickmick117@hotmail.com>
2018-03-29 13:08:33 -0400

    Second

---
+++ UnityGuitar/Assets/Scripts/Chord.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chord : MonoBehaviour
+{
+    public GameObject[] notes;
+
+    public Guitar guitar;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        guitar.ClearGuitar();
+        for (int i = 0; i < notes.Length; i++)
+        {
+            notes[i].GetComponent<SpriteRenderer>().enabled = !notes[i].GetComponent<SpriteRenderer>().enabled;
+        }
+        guitar.PlayStrings();
+    }
+}
